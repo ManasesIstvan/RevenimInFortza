5d26ccaa48589fd3f13c89c768b63ad344054d34
ManasesIstvan <figoistvan@yahoo.com>
2022-01-27 17:18:45 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+import java.util.Optional;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        String myString = "Manases Istvan";
+        int myint = -5;
+        int my2ndint = 8;
+        int my3rdint = 6;
+
+        System.out.println(myint + my2ndint * my3rdint);
+
+        int my4thint = 55;
+        int my5thint = 9;
+
+        System.out.println( (my4thint+my5thint) / my5thint );
+
+        int my6thint = 20;
+        int my7thint = -3;
+        int my12int = 5;
+
+        System.out.println(my6thint + my7thint * my12int / my2ndint);
+
+        int my8thint = 15;
+        int my9thint = 3;
+        int my10int = 2;
+
+        System.out.println(my12int + my8thint / my9thint * my10int - my2ndint % my9thint );
+
+
+
+    }
+
+
+
+}
